caa56a0aa607df9eeb1b658666330235bf72f8a2
Mr.Pashkins <shkurykhinpasha@gmail.com>
2016-03-28 05:26:31 +0300

    Not really final commit, but anyway=)

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,9 +2,11 @@ class Article < ActiveRecord::Base
   has_many :comments, dependent: :destroy
   validates :title, presence: true,
             length: { minimum: 5 }
+  validates :text, presence: true
 
   def self.search(search)
-    where("title LIKE ?", "%#{search}%")
-    where("text LIKE ?", "%#{search}%")
+    #where("title LIKE ?", "%#{search}%")
+   # where("text LIKE ?", "%#{search}%")
+    where("title LIKE ? OR text LIKE ?", "%#{search}%", "%#{search}%")
   end
 end
